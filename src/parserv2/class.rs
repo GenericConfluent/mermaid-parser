014779f4bd44c0a4e37642e35f12695c0e608617
GenericConfluent <m13power@gmail.com>
2025-11-10 13:40:47 -0700

    feat: parserv2 start class tests

--- src/parserv2/class.rs
+++ src/parserv2/class.rs
@@ -21,10 +21,22 @@ pub fn class_stmt(s: &str) -> IResult<&str, Stmt> {
     //      | noteStatement NEWLINE                     {$$=[[], [$1]]}
     //      | noteStatement NEWLINE classStatements     {$3[1].unshift($1); $$=$3}
     //      ;
+    //
+
+    // members
+    //     : MEMBER { $$ = [$1]; }
+    //     | MEMBER members { $2.push($1);$$=$2;}
+    //     ;
+    //  mermaid doesn't actually care about the structure of the class members too much. But we do
+    //  So we need parsing logic for them.
     let mut members = Vec::new();
+
+    todo!();
 
     Ok((
         s,
+        // NOTE: We don't want to go as far as parsing type generics, annotations, and we can't
+        // store namespace.
         Stmt::Class(Class {
             name: name.to_string(),
             generic: None,
@@ -49,6 +61,27 @@ pub fn class_member_stmt(s: &str) -> IResult<&str, Class> {
 //     | classLiteralName GENERICTYPE { $$=$1+'~'+$2+'~'; }
 //     ;
 // We don't care about generic though.
+// NOTE: alphaNumToken  : UNICODE_TEXT | NUM | ALPHA | MINUS;
 pub fn class_name(s: &str) -> IResult<&str, &str> {
     todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_class_name() {
+        let (rem, name) =
+            class_name("Normal23Class-Name").expect("Failed to parse alpha num tokens");
+        assert!(rem.is_empty());
+        assert_eq!(name, "Normal23Class-Name");
+
+        let (rem, name) = class_name("\t \t Whitespace  ").expect("Failed to parse whitespace");
+        assert!(rem.is_empty());
+        assert_eq!(name, "Whitespace");
+    }
+
+    #[test]
+    fn test_class_stmt() {}
 }
